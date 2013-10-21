a421248baaad4e9f9fbaf6909a7481414e9e55fa
unknown <josemg08@gmail.com>
2013-10-21 20:38:03 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flipper::Application.config.secret_token = '8c7b7fb587cff163b86637245dd506d90a15f5efa1de21be8636819ba343243117ff4ee7ff5973ba63df164e03b03675dad5f3c9735b2ba319384ee0c11df808'
